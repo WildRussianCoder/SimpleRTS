a04cf5916296eb4b27219300439aa4e9785b61fa
Igor Zhigulin <mailprogera@gmail.com>
2020-03-25 12:16:20 +0300

    Abstract class to Interface
    Сделал GraphicalObject интерфейсом, а не абстрактным классом. Теперь
    придётся реализовывать каждому графическому объекту все методы из
    GraphicalObject. Не знаю насколько это целесообразно, но мне показалось,
    что это более верное решение, чем абстрактный класс. Может быть, потом
    пересмотрю своё решение.

--- SimpleRTS/src/com/vk/superhellstrike/graphics/BasicQuad.java
+++ SimpleRTS/src/com/vk/superhellstrike/graphics/BasicQuad.java
@@ -2,8 +2,73 @@ package com.vk.superhellstrike.graphics;
 
 import java.awt.Rectangle;
 
-public class BasicQuad extends GraphicalObject {
+import com.jogamp.opengl.GL2;
+
+public class BasicQuad implements GraphicalObject {
+	private Rectangle r;
+	
 	public BasicQuad(int x, int y) {
 		this.r = new Rectangle(x - 10, y - 10, 20, 20);
+	}
+
+	@Override
+	public int getX() {
+		return r.x;
+	}
+
+	@Override
+	public int getY() {
+		return r.y;
+	}
+
+	@Override
+	public int getWidth() {
+		return r.width;
+	}
+
+	@Override
+	public int getHeight() {
+		return r.height;
+	}
+
+	@Override
+	public Rectangle getRectangle() {
+		return r;
+	}
+
+	@Override
+	public void setX(int x) {
+		r.x = x;
+	}
+
+	@Override
+	public void setY(int y) {
+		r.y = y;	
+	}
+
+	@Override
+	public void setWidth(int width) {
+		r.width = width;
+	}
+
+	@Override
+	public void setHeight(int height) {
+		r.height = height;
+		
+	}
+
+	@Override
+	public void draw(GL2 gl) {
+		gl.glRecti(r.x, r.y, r.x + r.width, r.y + r.height);
+	}
+
+	@Override
+	public boolean collisionCheck(GraphicalObject go) {
+		return r.contains(go.getRectangle());
+	}
+
+	@Override
+	public boolean contains(int x, int y) {
+		return r.contains(x, y);
 	}
 }
--- SimpleRTS/src/com/vk/superhellstrike/graphics/GraphicalObject.java
+++ SimpleRTS/src/com/vk/superhellstrike/graphics/GraphicalObject.java
@@ -4,83 +4,17 @@ import java.awt.Rectangle;
 
 import com.jogamp.opengl.GL2;
 
-public abstract class GraphicalObject {
-	protected Rectangle r;
-	
-	/*
-	 * Constructors
-	 */
-	public GraphicalObject() {
-		r = new Rectangle(0, 0, 1, 1);
-	}
-	
-	public GraphicalObject(int width, int height) {
-		r = new Rectangle(0, 0, width, height);
-	}
-	
-	public GraphicalObject(int x, int y, int width, int height) {
-		r = new Rectangle(x, y, width, height);
-	}
-	
-	/*
-	 * Getters
-	 */
-	public int getX() {
-		return r.x;
-	}
-	
-	public int getY() {
-		return r.y;
-	}
-	
-	public int getWidth() {
-		return r.width;
-	}
-	
-	public int getHeight() {
-		return r.height;
-	}
-	
-	public Rectangle getREctangle() {
-		return r;
-	}
-	
-	/*
-	 * Setters
-	 */
-	public void setX(int x) {
-		r.x = x;
-	}
-	
-	public void setY(int y) {
-		r.y = y;
-	}
-	
-	public void setWidth(int width) {
-		r.width = width;
-	}
-	
-	public void setHeight(int height) {
-		r.height = height;
-	}
-	
-	/*
-	 * Other methods
-	 */
-	public void draw(GL2 gl) {
-		gl.glRecti(r.x, r.y, r.x + r.width, r.y + r.height);
-	}
-	
-	public boolean collisionCheck(GraphicalObject o) {
-		boolean collision = false;
-		
-		if(this.r.contains(o.r))
-			collision = true;
-		
-		return collision;
-	}
-	
-	public boolean contains(int x, int y) {
-		return r.contains(x, y);
-	}
+public interface GraphicalObject {	
+	public int getX();
+	public int getY();
+	public int getWidth();
+	public int getHeight();
+	public Rectangle getRectangle();
+	public void setX(int x);
+	public void setY(int y);
+	public void setWidth(int width);
+	public void setHeight(int height);
+	public void draw(GL2 gl);
+	public boolean collisionCheck(GraphicalObject o);
+	public boolean contains(int x, int y);
 }
